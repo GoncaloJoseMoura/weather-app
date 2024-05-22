2c7f0a112a1b7f33279b96ada3565c36c4029412
GoncaloJoseMoura <43656133+GoncaloJoseMoura@users.noreply.github.com>
2024-05-22 02:31:48 +0100

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: path.resolve(__dirname, './src/index.js'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            // favicon: "./src/assets/favicon.svg",
+            filename: 'index.html',
+            inject: 'head',
+            scriptLoading: 'defer',
+            title: 'Production',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
